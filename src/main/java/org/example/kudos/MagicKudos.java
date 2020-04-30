79a3bc7c64765f5e924314913aa77165dfe7ccdd
vhugo <victor.hugo@8x8.com>
2020-04-30 14:20:53 -0300

    Adding swagger to the project

--- src/main/java/org/example/kudos/MagicKudos.java
+++ src/main/java/org/example/kudos/MagicKudos.java
@@ -2,11 +2,37 @@ package org.example.kudos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MagicKudos {
 
     public static void main(String[] args) {
         SpringApplication.run(MagicKudos.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfiguration() {
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/**"))
+               // .paths(PathSelectors.ant("/kudos/*"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("MagicKudo REST API")
+                .description("Simple example of API`s")
+                .version("1.0")
+                .build();
     }
 }
