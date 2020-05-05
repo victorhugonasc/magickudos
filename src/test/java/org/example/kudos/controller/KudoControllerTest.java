fec0340095794f9d1853dd076523142f8d475a35
vhugo <victor.hugo@8x8.com>
2020-05-05 15:47:10 -0300

    splitting up unit tests from integration tests

--- src/test/java/org/example/kudos/controller/KudoControllerTest.java
+++ src/test/java/org/example/kudos/controller/KudoControllerTest.java
@@ -3,48 +3,32 @@ package org.example.kudos.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.kudos.model.Kudo;
-import org.example.kudos.repository.KudoRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
-import static javafx.beans.binding.Bindings.when;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-@RunWith(PowerMockRunner.class)
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @WebMvcTest(controllers = KudoController.class)
 public class KudoControllerTest {
+
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup().build();
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @Mock
-    private KudoRepository kudoRepository;
-
-    @InjectMocks
-    KudoController kudoController;
-
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(kudoController).build();
-    }
 
     @Test
     public void testCreateKudo() throws Exception {
@@ -52,7 +36,7 @@ public class KudoControllerTest {
         Kudo kudoMock = new Kudo("9a99999a99aa999999999a9a", "victorTest", "testVictor", "ola, teste", "isso eh um teste");
         mockMvc.perform( MockMvcRequestBuilders.post("/kudos")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(kudoMock))
+                //.content(objectMapper.writeValueAsString(kudoMock))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
---
+++ src/test/java/org/example/kudos/controller/KudoControllerUnitTest.java
@@ -0,0 +1,34 @@
+package org.example.kudos.controller;
+
+import org.example.kudos.model.Kudo;
+import org.example.kudos.repository.KudoRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.testng.annotations.Test;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(PowerMockRunner.class)
+public class KudoControllerUnitTest {
+    @Mock
+    private KudoRepository kudoRepository;
+
+    @InjectMocks
+    KudoController kudoController;
+
+    HttpServletResponse response;
+
+    @Test
+    public void testSaveKudo() {
+        Kudo kudo = mock(Kudo.class);
+        kudoController.createKudo(kudo,response);
+        verify(kudoRepository.save(kudo));
+    }
+}
