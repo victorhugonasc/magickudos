5d066a60f8113b1186e5bcdf5b19ac438591e2ba
vhugo <victor.hugo@8x8.com>
2020-05-11 15:38:05 -0300

    Unit tests in progress

--- src/test/java/org/example/kudos/controller/KudoControllerUnitTest.java
+++ src/test/java/org/example/kudos/controller/KudoControllerUnitTest.java
@@ -4,35 +4,80 @@ import org.example.kudos.model.Kudo;
 import org.example.kudos.repository.KudoRepository;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class KudoControllerUnitTest {
-    @Mock
-    private KudoRepository kudoRepository;
 
-    @InjectMocks
-    KudoController kudoController;
+    @Autowired
+    private KudoController kudoController;
+
+    @MockBean
+    private KudoRepository kudoRepository;
 
     @Mock
     HttpServletResponse response;
 
     @Test
-    public void testSaveKudo() {
-        Kudo kudo = mock(Kudo.class);
+    public void testSaveKudos() {
+        Kudo kudoMock = mock(Kudo.class);
+        Kudo kudo = new Kudo("id","sender","receiver","message","layout");
+        when(kudoRepository.save(kudo)).thenReturn(kudoMock);
 
-        doNothing().when(kudoController.createKudo(any(),any()));
-       // doNothing().when(kudoRepository.save(any(Kudo.class)));
+      //  verify(kudoMock,times(1));
 
-        kudoController.createKudo(kudo,response);
-        verify(kudoRepository.save(kudo));
     }
+
+    @Test
+    public void testGetKudos() {
+        when(kudoRepository.findAll()).thenReturn(Stream.of(new Kudo("id","sender","receiver","message","layout")).collect(Collectors.toList()));
+        assertEquals(1,kudoController.getKudos().size());
+    }
+
+    @Test
+    public void testGetSingleKudo() {
+        Kudo kudoMock = mock(Kudo.class);
+        Kudo kudo = new Kudo("id","sender","receiver","message","layout");
+        //kudoRepository.findById(id).isPresent()
+        when(kudoRepository.findById("id").get()).thenReturn(kudoMock);
+
+    }
+
+    @Test
+    public void testWhenGetSingleKudoReturnsNull() {
+        //Kudo kudoMock = mock(Kudo.class);
+        Kudo kudo = new Kudo("1L","sender","receiver","message","layout");
+        kudoRepository.save(kudo);
+        //when(kudoRepository.save(kudo)).thenReturn(kudoMock);
+        when(kudoRepository.findById("1L").get()).thenReturn(kudo);
+
+    }
+
+
+    @Test
+    public void testStoreKudos() {
+        Kudo kudo = new Kudo("id","sender","receiver","message","layout");
+        when(kudoRepository.save(kudo)).thenReturn(kudo);
+        int index = 0;
+       // when(kudoRepository.findAll().get()).thenReturn(Stream.of(new Kudo("id","sender","receiver","message","layout")).collect(Collectors.toList()));
+        when(kudoRepository.findAll().get(index)).thenReturn(kudo);
+        assertEquals(1,kudoController.getKudos().size());
+    }
+
+
 }
