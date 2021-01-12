8de0a81df5e1a134195e14a9c63fab1a200396ab
donascvh <victorhugo.donascimento@bt.com>
2021-01-12 09:53:17 -0300

    Color endpoint

--- src/main/java/org/example/kudos/controller/ColorController.java
+++ src/main/java/org/example/kudos/controller/ColorController.java
@@ -1,10 +1,12 @@
 package org.example.kudos.controller;
 
+import org.example.kudos.model.Color;
 import org.example.kudos.repository.ColorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping("/colors")
@@ -16,6 +18,40 @@ public class ColorController {
 
     public ColorController() {
         super();
+    }
+
+    @GetMapping
+    public List<Color> getColors()
+    { return colorRepository.findAll(); }
+
+    @GetMapping(path = "/{kudosType}")
+    public Color getOneColorPallete(@PathVariable String kudosType, HttpServletResponse response)
+    {
+        if (colorRepository.findById(kudosType).isPresent())
+        {
+            return colorRepository.findById(kudosType).get();
+        }
+        response.setStatus(204);
+        return null;
+    }
+
+    @PostMapping()
+    public int createColor(@RequestBody Color color, HttpServletResponse response)
+    {
+        if (color.getKudosType()!= null && color.getHeaderMessage()!= null && color.getLogoImage() != null &&
+            color.getButton() != null && color.getButtonText() != null && color.getButtonHover() != null && color.getLogin() != null &&
+            color.getLoginBoxShallow() != null && color.getFieldsBoxShallow() != null) {
+            colorRepository.save(color);
+            response.setStatus(201);
+        }
+        else { response.setStatus(400); }
+        return response.getStatus();
+    }
+
+    @DeleteMapping()
+    public void deleteAllColors()
+    {
+        colorRepository.deleteAll();
     }
 
 }
--- src/main/java/org/example/kudos/model/Color.java
+++ src/main/java/org/example/kudos/model/Color.java
@@ -1,20 +1,53 @@
 package org.example.kudos.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import javax.validation.constraints.NotBlank;
 
-@AllArgsConstructor
-@Getter
-@Setter
 public class Color {
+
     @Id
     @NotBlank()
     private String kudosType;
     @NotBlank()
+    private String headerMessage;
+    @NotBlank()
     private String logoImage;
     @NotBlank()
     private String button;
+    @NotBlank()
+    private String buttonText;
+    @NotBlank()
+    private String buttonHover;
+    @NotBlank()
+    private String login;
+    @NotBlank()
+    private String loginBoxShallow;
+    @NotBlank()
+    private String fieldsBoxShallow;
+
+    public Color(@NotBlank() String kudosType, @NotBlank() String headerMessage, @NotBlank() String logoImage,
+                 @NotBlank() String button, @NotBlank() String buttonText, @NotBlank() String buttonHover,
+                 @NotBlank() String login, @NotBlank() String loginBoxShallow, @NotBlank() String fieldsBoxShallow) {
+        this.kudosType = kudosType;
+        this.headerMessage = headerMessage;
+        this.logoImage = logoImage;
+        this.button = button;
+        this.buttonText = buttonText;
+        this.buttonHover = buttonHover;
+        this.login = login;
+        this.loginBoxShallow = loginBoxShallow;
+        this.fieldsBoxShallow = fieldsBoxShallow;
+    }
+
+    public String getKudosType() { return kudosType; }
+    public String getHeaderMessage() { return headerMessage; }
+    public String getLogoImage() {return logoImage; }
+    public String getButton() { return button; }
+    public String getButtonText() { return buttonText; }
+    public String getButtonHover() { return buttonHover; }
+    public String getLogin() { return login; }
+    public String getLoginBoxShallow() { return loginBoxShallow; }
+    public String getFieldsBoxShallow() { return fieldsBoxShallow; }
 }
+
+
